4ae2268d18b201508c64efa6448627d1deced55e
Anand <hianandhowru@gmail.com>
2023-09-30 08:29:23 +0530

    Stage 1/5

---
+++ lastpencil/LastPencil.java
@@ -0,0 +1,10 @@
+package lastpencil;
+
+public class LastPencil {
+
+    public static void start() {
+        String verticalBar = "|||";
+        String yourTurn = "Your turn!";
+        System.out.printf("%s\n%s", verticalBar, yourTurn);
+    }
+}
--- lastpencil/Main.java
+++ lastpencil/Main.java
@@ -2,7 +2,6 @@ package lastpencil;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-
+        LastPencil.start();
     }
 }
