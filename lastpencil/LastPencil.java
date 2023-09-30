199235adfc044f1c062afdfe734df20ed478ac72
Anand <hianandhowru@gmail.com>
2023-09-30 13:24:47 +0530

    Stage 3/5

--- lastpencil/LastPencil.java
+++ lastpencil/LastPencil.java
@@ -1,11 +1,13 @@
 package lastpencil;
 
 import java.util.Scanner;
+import java.util.WeakHashMap;
 
 public class LastPencil {
 
     public static int NoOfPencil;
-    public static String playerName;
+    public static String playerFirst;
+    public static String playerSecond;
 
     public static void start() {
         Scanner scanner = new Scanner(System.in);
@@ -13,17 +15,38 @@ public class LastPencil {
         System.out.println("How many pencils would you like to use:");
         NoOfPencil = scanner.nextInt();
 
-        System.out.println("Who will be the first (John, Jack):");
-        playerName = scanner.next();
+        System.out.println("Who will be the first (John,Jack):");
+        playerFirst = scanner.next();
 
-        printPencils();
-        System.out.print(playerName + "is going first!");
+        playerSecond = playerFirst.equals("John") ? "Jack" : "John";
+
+        playGame();
+
     }
 
-    private static void printPencils() {
+    public static void printRemainingPencils(int counter) {
         for (int i=0; i<NoOfPencil; i++) {
             System.out.print("|");
         }
         System.out.println();
+        String currentPlayer;
+        if (counter % 2 != 0) {
+            currentPlayer = playerFirst;
+        } else {
+            currentPlayer = playerSecond;
+        }
+        System.out.println(currentPlayer + "'s turn:");
+    }
+
+    public static void playGame() {
+        Scanner scanner = new Scanner(System.in);
+
+        int counter = 1;
+        while (NoOfPencil > 0) {
+            printRemainingPencils(counter);
+            NoOfPencil -= scanner.nextInt();
+            counter++;
+        }
+
     }
 }
