a0514a70d2491496bfbba1812a7127a4f2bbf829
Anand <hianandhowru@gmail.com>
2023-09-30 10:32:02 +0530

    Stage 2/5

--- lastpencil/LastPencil.java
+++ lastpencil/LastPencil.java
@@ -1,10 +1,29 @@
 package lastpencil;
 
+import java.util.Scanner;
+
 public class LastPencil {
 
+    public static int NoOfPencil;
+    public static String playerName;
+
     public static void start() {
-        String verticalBar = "|||";
-        String yourTurn = "Your turn!";
-        System.out.printf("%s\n%s", verticalBar, yourTurn);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many pencils would you like to use:");
+        NoOfPencil = scanner.nextInt();
+
+        System.out.println("Who will be the first (John, Jack):");
+        playerName = scanner.next();
+
+        printPencils();
+        System.out.print(playerName + "is going first!");
+    }
+
+    private static void printPencils() {
+        for (int i=0; i<NoOfPencil; i++) {
+            System.out.print("|");
+        }
+        System.out.println();
     }
 }
